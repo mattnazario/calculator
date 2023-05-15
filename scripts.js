a88bfe890747286cf1f0794f7cca4f6538d93b97
mattnazario <mattnazario@gmail.com>
2023-05-15 08:35:01 -0500

    Add buttons and operate function

--- scripts.js
+++ scripts.js
@@ -10,3 +10,20 @@ function multiply(a, b){
 function divide(a, b){
     return a/b;
 }
+
+function operate(a, b, operator){
+    switch(operator){
+        case '-':
+            return subtract(a,b);
+        case '+':
+            return add(a,b);
+        case '*':
+            return multiply(a,b);
+        case '/':
+            return divide(a,b);
+    }
+}
+
+let n1;
+let operator;
+let n2;
