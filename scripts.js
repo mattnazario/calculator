a9b9a43b68a33f833d9078ae0c32943dfda2ff9d
mattnazario <mattnazario@gmail.com>
2023-05-15 09:28:35 -0500

    Add functionality to buttons

--- scripts.js
+++ scripts.js
@@ -14,16 +14,67 @@ function divide(a, b){
 function operate(a, b, operator){
     switch(operator){
         case '-':
-            return subtract(a,b);
+            return subtract(Number(a),Number(b));
         case '+':
-            return add(a,b);
+            return add(Number(a),Number(b));
         case '*':
-            return multiply(a,b);
+            return multiply(Number(a),Number(b));
         case '/':
-            return divide(a,b);
+            return divide(Number(a),Number(b));
+        default:
+            return;
     }
 }
 
-let n1;
-let operator;
-let n2;
+function allClear(){
+    opNumber = 0;
+    n1 = "";
+    n2 = "";
+    operator = '';
+    console.log("CLEAR");
+    screenText.textContent = " ";
+}
+function screenClear(){
+    screenText.textContent = " ";
+}
+
+function clickedButton(e){
+    if(e.target.textContent === "CLEAR"){
+        allClear();
+        return;
+    }
+    if(e.target.textContent === "="){
+        n1 = operate(n1,n2,operator);
+        screenText.textContent = n1;
+        n2 = 0;
+        opNumber = 0;
+        operator = '';
+        return; 
+    }
+    if(e.target.textContent === '+' | e.target.textContent === '-' | e.target.textContent=== '*' | e.target.textContent === '/')
+    {
+        opNumber++;
+        operator = e.target.textContent;
+    }else{
+        if(opNumber === 0){
+            n1+= e.target.textContent;
+        }
+        if(opNumber === 1){
+            n2+= e.target.textContent;
+        }
+    }
+    console.log(`n1 = ${n1}`);
+    console.log(`n2 = ${n2}`);
+    console.log(`operator = ${operator}`);
+
+    screenText.textContent+=e.target.textContent;
+}
+buttons = document.querySelectorAll("button");
+buttons.forEach( btn => btn.addEventListener("click", clickedButton));
+console.log(buttons);
+
+let screenText = document.querySelector(".screen");
+let opNumber = 0;
+let n1 = "";
+let operator = ""
+let n2 = "";
